adventuregamestudio/ags

commit 13a5e167ea8ae3a762647c39b39c0e3a58fdee61

Engine: keep track of DrawingSurface connection to DynamicSprite
Ensure there's only 1 instance of DrawingSurface for the DynamicSprite at the same time.
Invalidate DrawingSurface on DynamicSprite's deletion.

Engine/ac/dynamicsprite.cpp
@@ -47,10 +47,32 @@ extern AGS::Engine::IGraphicsDriver *gfxDriver;
 
 char check_dynamic_sprites_at_exit = 1;
 
+// Dynamic sprite (slot index) map to DrawingSurface handle.
+// NOTE: I decided to put this separate from the ScriptDynamicSprite, because
+// statistically a reference to drawing surface is a temporary data present
+// in very small numbers (1-2) at one time; while there could be many hundreds
+// and thousands of dynamic sprites in game. Thus keeping a DrawingSurface ref
+// field in ScriptDynamicSprite will increase memory consumption in rates
+// which are highly unproportional to the actual needs.
+// TODO: of course this should not be a standalone global variable,
+// but put into some kind of a manager.
+std::unordered_map<int, int> DynamicSpriteDSRef;
+
 // ** SCRIPT DYNAMIC SPRITE
 
-void DynamicSprite_Delete(ScriptDynamicSprite *sds) {
-    if (sds->slot) {
+void DynamicSprite_Delete(ScriptDynamicSprite *sds)
+{
+    if (sds->slot > 0)
+    {
+        // Invalidate DrawingSurface (if one is currently acquired)
+        auto ref_it = DynamicSpriteDSRef.find(sds->slot);
+        if (ref_it != DynamicSpriteDSRef.end())
+        {
+            ScriptDrawingSurface *surf = static_cast<ScriptDrawingSurface *>(ccGetObjectAddressFromHandle(ref_it->second));
+            if (surf)
+                surf->Invalidate();
+        }
+
         free_dynamic_sprite(sds->slot);
         sds->slot = 0; // 0 is a "safety placeholder" sprite id
     }
@@ -58,7 +80,14 @@ void DynamicSprite_Delete(ScriptDynamicSprite *sds) {
 
 ScriptDrawingSurface* DynamicSprite_GetDrawingSurface(ScriptDynamicSprite *dss)
 {
-    game.SpriteInfos[dss->slot].Flags |= SPF_SURFACEACQUIRED;
+    // If there's already a active DrawingSurface, then return one
+    auto ref_it = DynamicSpriteDSRef.find(dss->slot);
+    if (ref_it != DynamicSpriteDSRef.end())
+    {
+        ScriptDrawingSurface *surface = static_cast<ScriptDrawingSurface*>(ccGetObjectAddressFromHandle(ref_it->second));
+        if (surface)
+            return surface;
+    }
 
     ScriptDrawingSurface *surface = new ScriptDrawingSurface();
     surface->dynamicSpriteNumber = dss->slot;
@@ -66,7 +95,10 @@ ScriptDrawingSurface* DynamicSprite_GetDrawingSurface(ScriptDynamicSprite *dss)
     if ((game.SpriteInfos[dss->slot].Flags & SPF_ALPHACHANNEL) != 0)
         surface->hasAlphaChannel = true;
 
-    ccRegisterManagedObject(surface, surface);
+    int handle = ccRegisterManagedObject(surface, surface);
+    DynamicSpriteDSRef[dss->slot] = handle;
+    // Mark the sprite info as having sprite's surface acquired (for notification purposes)
+    game.SpriteInfos[dss->slot].Flags |= SPF_SURFACEACQUIRED;
     return surface;
 }
 
@@ -455,7 +487,8 @@ int add_dynamic_sprite(int slot, std::unique_ptr<Bitmap> image, bool has_alpha, 
     return slot;
 }
 
-void free_dynamic_sprite(int slot, bool notify_all) {
+void free_dynamic_sprite(int slot, bool notify_all)
+{
     assert((slot > 0) && (static_cast<size_t>(slot) < game.SpriteInfos.size()) &&
         (game.SpriteInfos[slot].Flags & SPF_DYNAMICALLOC));
     if ((slot <= 0) || (static_cast<size_t>(slot) >= game.SpriteInfos.size()) ||
@@ -467,6 +500,18 @@ void free_dynamic_sprite(int slot, bool notify_all) {
         game_sprite_updated(slot, true);
     else
         notify_sprite_changed(slot, true);
+}
+
+void on_dynsprite_surface_release(int slot, bool modified)
+{
+    if (modified)
+    {
+        game_sprite_updated(slot);
+    }
+
+    DynamicSpriteDSRef.erase(slot);
+    // FIXME: must track the number of created drawing surface instances in dynamic sprite!
+    game.SpriteInfos[slot].Flags &= ~SPF_SURFACEACQUIRED;
 }
 
 //=============================================================================

Engine/ac/draw.cpp
@@ -443,6 +443,7 @@ std::vector<SpriteListEntry> sprlist;
 
 // For raw drawing
 std::unique_ptr<Bitmap> raw_saved_screen;
+// Stores cloned surfaces (see DrawingSurface.CreateCopy)
 std::unique_ptr<Bitmap> dynamicallyCreatedSurfaces[MAX_DYNAMIC_SURFACES];
 
 

Engine/ac/draw.h
@@ -226,6 +226,7 @@ Common::Bitmap *CopyScreenIntoBitmap(int width, int height, const Rect *src_rect
 
 // TODO: hide these behind some kind of an interface
 extern std::unique_ptr<Common::Bitmap> raw_saved_screen;
+// Stores cloned surfaces (see DrawingSurface.CreateCopy)
 extern std::unique_ptr<Common::Bitmap> dynamicallyCreatedSurfaces[MAX_DYNAMIC_SURFACES];
 
 #endif // __AGS_EE_AC__DRAW_H

Engine/ac/drawingsurface.cpp
@@ -11,10 +11,11 @@
 // https://opensource.org/license/artistic-2-0/
 //
 //=============================================================================
-#include "ac/draw.h"
 #include "ac/drawingsurface.h"
 #include "ac/common.h"
 #include "ac/display.h"
+#include "ac/draw.h"
+#include "ac/dynamicsprite.h"
 #include "ac/game.h"
 #include "ac/gamesetupstruct.h"
 #include "ac/gamestate.h"
@@ -77,13 +78,7 @@ void DrawingSurface_Release(ScriptDrawingSurface* sds)
     }
     else if (sds->dynamicSpriteNumber >= 0)
     {
-        if (sds->modified)
-        {
-            game_sprite_updated(sds->dynamicSpriteNumber);
-        }
-
-        // FIXME: must track the number of created drawing surface instances in dynamic sprite!
-        game.SpriteInfos[sds->dynamicSpriteNumber].Flags &= ~SPF_SURFACEACQUIRED;
+        on_dynsprite_surface_release(sds->dynamicSpriteNumber, sds->modified);
         sds->dynamicSpriteNumber = -1;
     }
     else if (sds->dynamicSurfaceNumber >= 0)

Engine/ac/dynamicsprite.h
@@ -55,5 +55,7 @@ int     add_dynamic_sprite(std::unique_ptr<AGS::Common::Bitmap> image, bool has_
 int     add_dynamic_sprite(int slot, std::unique_ptr<AGS::Common::Bitmap> image, bool has_alpha = false, uint32_t extra_flags = 0u);
 // Disposes a dynamic sprite, and frees the slot
 void    free_dynamic_sprite(int slot, bool notify_all = true);
+// Notifies dynamic sprite that its drawing surface was released
+void    on_dynsprite_surface_release(int slot, bool modified);
 
 #endif // __AGS_EE_AC__DYNAMICSPRITE_H

Engine/ac/dynobj/scriptdrawingsurface.cpp
@@ -42,7 +42,7 @@ Bitmap* ScriptDrawingSurface::GetBitmapSurface()
         return linkedBitmapOnly;
     else if (roomMaskType > kRoomAreaNone)
         return thisroom.GetMask(roomMaskType);
-    quit("!DrawingSurface: attempted to use surface after Release was called");
+    quit("!DrawingSurface: attempted to use surface after its source image was disposed or DrawingSurface.Release() was called.");
     return nullptr;
 }
 
@@ -113,7 +113,7 @@ void ScriptDrawingSurface::Unserialize(int index, Stream *in, size_t /*data_sz*/
     ccRegisterUnserializedObject(index, this, this);
 }
 
-ScriptDrawingSurface::ScriptDrawingSurface() 
+void ScriptDrawingSurface::Invalidate()
 {
     roomBackgroundNumber = -1;
     roomMaskType = kRoomAreaNone;
@@ -121,10 +121,16 @@ ScriptDrawingSurface::ScriptDrawingSurface()
     dynamicSurfaceNumber = -1;
     isLinkedBitmapOnly = false;
     linkedBitmapOnly = nullptr;
+    hasAlphaChannel = 0;
+}
+
+ScriptDrawingSurface::ScriptDrawingSurface() 
+{
+    Invalidate();
+
     currentColour = play.raw_color;
     currentColourScript = 0;
     modified = 0;
-    hasAlphaChannel = 0;
     highResCoordinates = 0;
     // NOTE: Normally in contemporary games coordinates ratio will always be 1:1.
     // But we still support legacy drawing, so have to set this up even for modern games,

Engine/ac/dynobj/scriptdrawingsurface.h
@@ -33,7 +33,6 @@ struct ScriptDrawingSurface final : AGSCCDynamicObject {
     int highResCoordinates;
     int modified;
     int hasAlphaChannel;
-    //Common::Bitmap* abufBackup;
 
     int Dispose(void *address, bool force) override;
     const char *GetType() override;
@@ -46,6 +45,8 @@ struct ScriptDrawingSurface final : AGSCCDynamicObject {
     void SizeToDataResolution(int *adjustValue);
     void FinishedDrawing();
     void FinishedDrawingReadOnly();
+
+    void Invalidate();
 
     ScriptDrawingSurface();
 

sysfce2/Game-Engine_adventure-game-studio

commit eb4f44b92559094ccd6bfc26df594da2452fe424

Engine: fixed surface tracking if sprite deleted before surf released
Mistake introduced by 13a5e167ea8ae3a762647c39b39c0e3a58fdee61

Engine/ac/dynamicsprite.cpp
@@ -71,6 +71,7 @@ void DynamicSprite_Delete(ScriptDynamicSprite *sds)
             ScriptDrawingSurface *surf = static_cast<ScriptDrawingSurface *>(ccGetObjectAddressFromHandle(ref_it->second));
             if (surf)
                 surf->Invalidate();
+            DynamicSpriteDSRef.erase(ref_it);
         }
 
         free_dynamic_sprite(sds->slot);